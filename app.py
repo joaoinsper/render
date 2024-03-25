15616d51fedb5b85723d1f97645322d7bc5fd609
joaoinsper <joaovsb2@insper.edu.br>
2024-03-25 12:15:56 -0300

    voltando ao original

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def uol():
         azinho = li.find('a')
         link = azinho.get('href')
         titulo = azinho.get('title')
-        manchetes_uol.append(f"{titulo}<br><a href='{link}'>{titulo}</a>")
+        manchetes_uol.append(f"{titulo}\n{link}")
     return render_template('uol.html', resultado = "\n\n".join(manchetes_uol))
 
 @app.route("/telegram", methods=["POST"])
